36a04391d654c259a0fcac990861385552302aad
Ganga Geoffrey (PCF5JWY) <pcf5jwy@wksp0009a345.hsd1.ga.comcast.net>
2018-07-12 20:49:38 -0400

    adding full project

---
+++ Templating2018/js/main.js
@@ -0,0 +1,212 @@
+
+$(document).ready(function(){
+		var getShit = $(document)
+		init();
+    
+    function init(){
+        container = $('#container');
+		win = $(window);
+        
+        $.fetcher([
+			["templates/landing.html"],
+			["templates/app.html"]
+		]).then(checkLoginState)
+        
+    }
+        
+    function checkLoginState(){
+        $.ajax({
+			url: 'xhr/check_login.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
+				if(response.error){
+					loadLanding(response);
+                    console.log("land - response",response)
+				}else{
+					loadApp(response);
+                    console.log(response)
+				}
+			}
+		});
+    }
+    
+    
+    function loadLanding(r){
+        
+        $.get('templates/landing.html', function(html){
+		
+			var h = $(html);
+			var landingCode = h.find('#template_landing').html();
+			
+			$.template('landing', landingCode);		// compile template
+			h = $.render(r.error, 'landing');		// use template
+			
+			container.html(h) 
+			
+		});
+    }
+    
+    
+    function loadApp(r){
+        $.get('templates/app.html', function(html){
+		
+			var h = $(html);
+			var appCode = h.find('#template_app').html();
+			
+			
+			$.template('app', appCode);		// compile template
+			h = $.render(r.user, 'app');		// use template
+										console.log(r.user)
+										console.log(r.user.id)//getting the user id
+			container.html(h) 
+			$('#welcomeUser').html('<p class="welcomeUser">'+r.user.user_n+'</p>')
+getProjects(appCode);
+//		getTasks();
+		});
+        
+    }
+
+    
+    function login(username,password){
+        $.ajax({
+			url: 'xhr/login.php',
+			type: 'post',
+			dataType: 'json',
+			data: {
+				username: username,
+				password: password
+			},
+			success: function(response){
+				console.log(response)
+				if(response.user){
+					
+					checkLoginState();
+				}else{
+					console.log('FAIL')			
+				};
+			}
+		});
+        
+    }
+ 
+    function logOut(){
+        $.ajax({
+			url: 'xhr/logout.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
+				if(response.success){
+					checkLoginState();
+				}	
+			}	
+		});
+    }
+    	
+function register(email,username,password){
+    $.ajax({
+			url: 'xhr/register.php',
+			type: 'post',
+			dataType: 'json',
+			data: {
+				email: email,
+                username: username,
+				password: password
+			},
+			success: function(response){
+//                console.log(response);
+                if(response.error){
+                    $('#registerErrorTxt').html('<p class="registerEr">'+response.error+'</p>')
+                }else{
+                    checkLoginState()
+                }
+
+			}
+		});
+}
+    function getProjects(appCode){
+        $.ajax({
+			url: 'xhr/get_projects.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
+                $.get('templates/app.html', function(html){
+                    for(var i =0; i<response.projects.length; i++){
+                        var allProjects = response.projects[i]
+//                        console.log("all Proj:",allProjects)
+			         }
+                    var project_tmpl = $(html).find('#projectTemplate').html();
+                    $.template('project', project_tmpl);
+                    var html = $.render(response.projects, 'project');
+                    $(".projectsData").append(html)
+                })
+		  }
+        })
+    }
+    
+    function getTasks(projectClicked){
+        $.ajax({
+			url: 'xhr/get_tasks.php',
+			type: 'get',
+			dataType: 'json',
+            data: {
+				projectID: projectClicked
+			},
+			success: function(response){
+            $.get('templates/projectDetails.html', function(html){
+                for(var i =0; i<response.tasks.length; i++){
+                        var allTasks = response.tasks[i]
+                        console.log("all Proj:", allTasks)
+			         }
+//				console.log("tasks",response)
+                
+			})
+            }
+		})
+    }
+
+
+
+
+$(document).on('click', '.loginBTN', function(e){
+    $("#loginModal").on('hidden.bs.modal', function (e) {
+            var username = $("#userName").val()
+            var password = $("#password").val()
+//         console.log(username)  
+        login(username,password)
+    })
+});
+
+$(document).on('click', '.registerBTN', function(e){
+    $("#registerModal").on('hidden.bs.modal', function (e) {
+            var email = $("#exampleInputEmail1").val()
+            var username = $("#exampleInputUsername").val()
+            var password = $("#exampleInputPassword1").val()
+//         console.log(username)  
+        register(email,username,password)
+    })
+});
+
+$(document).on('click', '.logOutBtn', function(){
+   logOut()
+});
+    
+$(document).on('click', ".projectRow", function(e){
+    var projectClicked = this.id
+     console.log(this)
+    getTasks(projectClicked)
+    
+});
+
+	
+	
+	
+
+		
+	}); // end document ready
+	
+
+
+
+
+
